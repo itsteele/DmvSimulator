e943c46ca1c285ed215a9ee51af5c711ec5c97d3
Isaac Steele <isaacsteele@Isaacs-MacBook-Pro.local>
2021-03-02 06:54:03 -0500

    Added an increased wait time for the user and changed a print statement to make more sense.

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -2,25 +2,25 @@ public class DmvSimulator {
 	
 	public static void main(String[] args) {
 		
-		int spot = (int) (Math.random() * (100 - 1)) + 1;
+		int spot = (int) (Math.random() * (200 - 100)) + 100;
 		
 		System.out.printf("Hello, welcome to the DMV! \nYou're spot in line is %d \nPlease wait until your number is called\n", spot);
 		
 		int x = spot + 1;
 		int y = 0;
 		
-		while (x > spot & x < 100) {
+		while (x > spot & x < 200) {
 			System.out.printf("Number %d\n", x);
 				x++;
 		}
 		
-		if (x == 100) {
+		if (x == 200) {
 			while (y <= spot) {
 			System.out.printf("Number %d\n", y);
 			y++;
 			
 				if (y == spot - 1) {
-					System.out.println("Number " + spot + ": You do not have the requirements.");
+					System.out.println("Number " + spot + ": You do not have the required paperwork.");
 					System.exit(0);
 				}
 			}
