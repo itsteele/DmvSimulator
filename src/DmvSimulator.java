8b3da36662924b90ae04b64409258024160aa1dd
Andrew231456 <andrewmatos203@gmail.com>
2021-03-02 17:57:51 -0500

    Changed print line once again for required paperwork with evil laugh

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -22,7 +22,7 @@ public class DmvSimulator {
 				if (y == spot - 1) {
 					int miracle = (int) (Math.random() * (100 - 1)) + 1;
 					if (miracle == 1) { System.out.println("You have the required paperwork. You're all set!"); }
-					else { System.out.println("Number " + spot + ": You do NOT have the required paperwork! Get the right paperwork and wait in line again!"); }
+					else { System.out.println("Number " + spot + ": You do not have the paperwork! That means you will have to wait in the long line again and suffer MUAHAHA!"); }
 					System.exit(0);
 				}
 			}
